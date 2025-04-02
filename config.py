0630ed2ce28021118beeed025f06620b66ca279c
Chiru6301 <chiruedizz@gmail.com>
2025-04-02 07:34:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class Config(object):
     API_HASH = "4693513c08d1ac6af15f95b116c29478"
     AUTH_USER = os.environ.get('AUTH_USERS', '7935947598').split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
-    HOST = "https://drm-api-six.vercel.app"
+    HOST = "http://master-api-v3.vercel.app"
     CREDIT = ""
