ed3c5fd9add53f6f290beedc3602a106d9d65307
Chiru6301 <chiruedizz@gmail.com>
2025-04-02 07:24:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = ""
-    API_ID = 
-    API_HASH = ""
-    AUTH_USER = os.environ.get('AUTH_USERS', '').split(',')
+    BOT_TOKEN = "7641591924:AAHZaHqvP5uEjnphGf3RhCkimVIGscKu6MU"
+    API_ID = 26468828
+    API_HASH = "4693513c08d1ac6af15f95b116c29478"
+    AUTH_USER = os.environ.get('AUTH_USERS', '7935947598').split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
-    HOST = "https://api.masterapi.tech"
+    HOST = "https://drm-api-six.vercel.app"
     CREDIT = ""
